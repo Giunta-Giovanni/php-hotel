dc6de7f79b2cba458f210713a604da639e968726
GiuntaGiovanni <jhonniest741@gmail.com>
2025-04-28 22:15:52 +0200

    [feat] add style on form & finish milestone

--- README.md
+++ README.md
@@ -10,10 +10,10 @@ Stampare una tabella con tutti gli hotel e i relativi dati disponibili.√
 - Dopo aggiungete Bootstrap e mostrate le informazioni con una tabella.√
 
 # Milestone 2 
--  Aggiungere un form ad inizio pagina che tramite una richiesta GET permetta di filtrare gli hotel che hanno un parcheggio.
+-  Aggiungere un form ad inizio pagina che tramite una richiesta GET permetta di filtrare gli hotel che hanno un parcheggio.√
 
 # Milestone 3
--  Aggiungere un secondo campo al form che permetta di filtrare gli hotel per voto (es. inserisco 3 ed ottengo tutti gli hotel che hanno un voto di tre stelle o superiore).
+-  Aggiungere un secondo campo al form che permetta di filtrare gli hotel per voto (es. inserisco 3 ed ottengo tutti gli hotel che hanno un voto di tre stelle o superiore).√
 
 # NOTA:
 Deve essere possibile utilizzare entrambi i filtri contemporaneamente (es. ottenere una lista con hotel che dispongono di parcheggio e che hanno un voto di tre stelle o superiore).
--- hotels.php
+++ hotels.php
@@ -53,108 +53,116 @@
 
 
 
-<!-- creiamo il form -->
-<form method = "GET">
-    <div>
-        <label> parcheggio </label>
-        <input type="radio" name = "need_parking" value=1>yes
-        <input type="radio" name = "need_parking" value=0>no
-    </div>
-    <div>
-    <label> vote </label>
-    <select name="vote" value="1">
-        <option value="1">1</option>
-        <option value="2">2</option>
-        <option value="3">3</option>
-        <option value="4">4</option>
-        <option value="5">5</option>
-    </select>
+    <!-- creiamo il form -->
+    <div class="container mt-4">
+        <form method="GET" class="p-4 border rounded bg-light">
+            <div class= "row">
+            <div class="col form-group">
+                <label class="font-weight-bold">Parcheggio</label>
+                <div class="col form-check">
+                    <input class="form-check-input" type="radio" name="need_parking" value="1" id="parkingYes">
+                    <label class="form-check-label" for="parkingYes">Yes</label>
+                </div>
+                <div class="col form-check">
+                    <input class="form-check-input" type="radio" name="need_parking" value="0" id="parkingNo">
+                    <label class="form-check-label" for="parkingNo">No</label>
+                </div>
+            </div>
 
+            <div class="col form-group">
+                <label class="font-weight-bold" for="vote">Vote</label>
+                <select class="form-control" name="vote" id="vote">
+                    <option value="1">1</option>
+                    <option value="2">2</option>
+                    <option value="3">3</option>
+                    <option value="4">4</option>
+                    <option value="5">5</option>
+                </select>
+            </div>
+            </div>
+            
+            <div class="d-flex justify-content-end">
+                <button type="submit" class="btn btn-dark">Invia</button>
+            </div>
+        </form>
 
-    </div>
+        <table class="table table-bordered table-striped">
+        <thead class="thead-dark">
+        <tr>
+            <?php 
+                // Otteniamo le chiavi del primo elemento dell'array ($hotels[0]) per usarle come intestazioni della tabella
+                $keys = array_keys($hotels[0]);
 
-
-    <button type="submit">invia</button>
-</form>
-
-
-<table class="table table-bordered table-striped">
-<thead class="thead-dark">
-<tr>
-    <?php 
-        // Otteniamo le chiavi del primo elemento dell'array ($hotels[0]) per usarle come intestazioni della tabella
-        $keys = array_keys($hotels[0]);
-
-        // Cicliamo su tutte le chiavi ottenute ricavandoci la singola chiave
-        foreach($keys as $key){
-            //stampiamo ogni chiave come una colonna dell'intestazione della tabella
-            echo "<th>$key</th>";
-        };
-    ?>
-</tr>
-</thead>   
-    <!-- riga di intestazione -->
-    <?php
-
-    //ricaviamoci i dati del form
-    // Need Parking
-    if(isset($_GET['need_parking'])){
-        // ci salviamo la richiesta del client
-        $needParking = intval($_GET["need_parking"]);
-    }else {
-        $needParking = null;
-    }
-
-    // Vote
-    $vote = intval($_GET['vote']);
-
-
-        // var_dump($needParking);
-
-
-        // mi creo un'array di hotel filtrati da riempire
-        $filteredHotels = [];
-
-        // ciclo sugli hotel e mi ricavo il singolo elemento
-        foreach($hotels as $hotel){
-            // se il cliente ha scelto il parcheggio
-            if($needParking != null){
-                //condizione sia sul parking che sul voto
-                if($hotel["parking"] == $needParking && $hotel["vote"] >= $vote){
-                    //inseriamo l'elemento all'interno dell'array creato
-                    array_push($filteredHotels, $hotel);
+                // Cicliamo su tutte le chiavi ottenute ricavandoci la singola chiave
+                foreach($keys as $key){
+                    //stampiamo ogni chiave come una colonna dell'intestazione della tabella
+                    echo "<th>$key</th>";
                 };
-                //se il cliente non ha scelto nessuna delle due
-            }else{
-                //condizione sul voto
-                if($hotel["vote"] >= $vote){
-                    //inseriamo l'elemento all'interno dell'array creato
-                    array_push($filteredHotels, $hotel);
+            ?>
+        </tr>
+        </thead>
+        <!-- riga di intestazione -->
+        <?php
+
+        //ricaviamoci i dati del form
+        // Need Parking
+        if(isset($_GET['need_parking'])){
+            // ci salviamo la richiesta del client
+            $needParking = intval($_GET["need_parking"]);
+        }else {
+            $needParking = null;
+        }
+
+        // Vote
+        $vote = intval($_GET['vote']);
+
+
+            // var_dump($needParking);
+
+
+            // mi creo un'array di hotel filtrati da riempire
+            $filteredHotels = [];
+
+            // ciclo sugli hotel e mi ricavo il singolo elemento
+            foreach($hotels as $hotel){
+                // se il cliente ha scelto il parcheggio
+                if($needParking != null){
+                    //condizione sia sul parking che sul voto
+                    if($hotel["parking"] == $needParking && $hotel["vote"] >= $vote){
+                        //inseriamo l'elemento all'interno dell'array creato
+                        array_push($filteredHotels, $hotel);
+                    };
+                    //se il cliente non ha scelto nessuna delle due
+                }else{
+                    //condizione sul voto
+                    if($hotel["vote"] >= $vote){
+                        //inseriamo l'elemento all'interno dell'array creato
+                        array_push($filteredHotels, $hotel);
+                    };
                 };
             };
-        };
 
-        // var_dump($filteredHotels);
+            // var_dump($filteredHotels);
 
-            // cicliamo su l'array filtrato per ricavare gli elementi
-            foreach($filteredHotels as $filteredHotel){
-            // apriamo la riga della tabella
-            echo "<tr>";
-    
-            // per ogni hotel(associativo) richiaviamoci la coppia chiave valore
-            foreach ($filteredHotel as $key => $value){
-                // per ogli colonna stampiamo il valore
-                echo "<td>$value</td>";
-            };
-            // chiudiamo la riga una volta finito i valori
-            echo "</tr>";
-            };
-    ?>
+                // cicliamo su l'array filtrato per ricavare gli elementi
+                foreach($filteredHotels as $filteredHotel){
+                // apriamo la riga della tabella
+                echo "<tr>";
+        
+                // per ogni hotel(associativo) richiaviamoci la coppia chiave valore
+                foreach ($filteredHotel as $key => $value){
+                    // per ogli colonna stampiamo il valore
+                    echo "<td>$value</td>";
+                };
+                // chiudiamo la riga una volta finito i valori
+                echo "</tr>";
+                };
+            ?>
+        </table>
+    </div>
 
 
 
-
-</table>
 
 </body>
 </html>
